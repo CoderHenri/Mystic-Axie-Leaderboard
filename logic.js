285493b4b9eca5d234bc37c717fc0f0d9a31dbcd
Henri0812 <p.metzner@gmx.at>
2020-05-06 21:02:28 +0200

    still some problems left

--- logic.js
+++ logic.js
@@ -50,11 +50,8 @@ async function GetMysticAxies() {
         AllMysticsURL = "https://axieinfinity.com/api/v2/axies?mystic=true&offset="+OffsetNumber+"&sorting=lowest_id";
         console.log(OffsetNumber);
         await GetMysticOwners(AllMysticsURL);
-        if(i == (OffsetAmount - 1)) {
-            console.log(MysticArrayOwners);
-            console.log(RareClassMysticArrayOwners);
-        }
     }
+    ListenSorter();
 }
 
 async function GetMysticOwners(url) {
@@ -66,11 +63,115 @@ async function GetMysticOwners(url) {
             var MysticID = data.axies[m].id;
             var MysticOwner = data.axies[m].owner;
             var MysticClass = data.axies[m].class;
-            MysticArrayOwners.push({Owner : MysticOwner, ID : MysticID});
+            MysticArrayOwners.push({OwnerETH : MysticOwner, ID : MysticID, amount : 1});
             console.log(MysticID + " IDDDDDDD");
             if(MysticClass == "bird" || MysticClass == "reptile" || MysticClass == "bug") {
-                RareClassMysticArrayOwners.push({RareClassOwner : MysticOwner, RareClassID : MysticID})
+                RareClassMysticArrayOwners.push({RareClassOwner : MysticOwner, RareClassID : MysticID, amount : 1})
             }
         } catch { continue; }
     }
+}
+
+function ListenSorter() {
+
+    var amount = [];
+    MysticArrayOwners.sort((a,b) => b.OwnerETH - a.OwnerETH);
+
+    for(p=0; MysticArrayOwners.length > p; p++) {
+        if(p > 0) {
+        if(MysticArrayOwners[p].OwnerETH == MysticArrayOwners[p-1].OwnerETH) {
+            amount[amount.length - 1].amount = amount[amount.length - 1].amount + MysticArrayOwners[p].amount;
+        } else {
+            amount.push({amount : MysticArrayOwners[p].amount, OwnerETH : MysticArrayOwners[p].OwnerETH});
+        }
+        } else {
+        amount.push({amount : MysticArrayOwners[p].amount, OwnerETH : MysticArrayOwners[p].OwnerETH});
+        }
+    }
+
+    amount.sort((a,b) => b.amount - a.amount || a.OwnerETH - b.OwnerETH);
+
+    var MysticAmount = [];
+
+    RareClassMysticArrayOwners.sort((a,b) => b.RareClassOwner - a.RareClassOwner);
+
+    for(p=0; RareClassMysticArrayOwners.length > p; p++) {
+        if(p > 0) {
+        if(RareClassMysticArrayOwners[p].RareClassOwner == RareClassMysticArrayOwners[p-1].RareClassOwner) {
+            MysticAmount[MysticAmount.length - 1].amount = MysticAmount[MysticAmount.length - 1].amount + RareClassMysticArrayOwners[p].amount;
+        } else {
+            MysticAmount.push({amount : RareClassMysticArrayOwners[p].amount, OwnerETH : RareClassMysticArrayOwners[p].RareClassOwner});
+        }
+        } else {
+            MysticAmount.push({amount : RareClassMysticArrayOwners[p].amount, OwnerETH : RareClassMysticArrayOwners[p].RareClassOwner});
+        }
+    }
+
+    MysticAmount.sort((a,b) => b.amount - a.amount || a.OwnerETH - b.OwnerETH);
+
+    console.log(amount);
+    console.log("bevor Profilenamer");
+    ProfileNamer(amount, MysticAmount);
+}
+
+async function ProfileNamer(Array, MysticArray) {
+  
+    var url = "https://axieinfinity.com/graphql-server/graphql"
+    
+    for(z=0; Array.length > z; z++) {
+      var ethAddy = Array[z].OwnerETH;
+      ethAddy = JSON.stringify(ethAddy);
+      var FetchChecker = "NEIN";
+      FetchChecker = "NEIN";
+    
+      for(n=0; NameArray.length > n; n++) {
+        if(NameArray[n].Eth == Array[z].OwnerETH) {
+          Array[z].OwnerETH = NameArray[n].Besitzer;
+          FetchChecker = "JA";
+          break;
+        }
+      }
+    
+      
+      if(FetchChecker == "NEIN") {
+          console.log("sucht");
+        await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+          },
+          
+          body: JSON.stringify({
+            operationName:"GetProfileByEthAddress",
+            variables:{
+              ethereumAddress:ethAddy
+            },
+            query:"query GetProfileByEthAddress($ethereumAddress: String!) {\n  publicProfileWithEthereumAddress(ethereumAddress: $ethereumAddress) {\n    ...Profile\n    __typename\n  }\n}\n\nfragment Profile on PublicProfile {\n  accountId\n  name\n  addresses {\n    ...Addresses\n    __typename\n  }\n  __typename\n}\n\nfragment Addresses on NetAddresses {\n  ethereum\n  tomo\n  loom\n  __typename\n}\n"})
+        })
+          
+        .then(function(response) { 
+          return response.json(); 
+        })
+     
+        .then(function(data) {
+          var realName = "";
+          try {
+            realName = data.data.publicProfileWithEthereumAddress.name;
+          }
+          catch {
+            realName = "No User Profile";
+          }
+          Array[z].OwnerETH = realName;
+        });
+      }
+    }
+    
+    ETHAddressNamer(Array, MysticArray);
+}
+
+function ETHAddressNamer(MysticArray, RareMysticArray) {
+    console.log(MysticArray);
+    console.log(RareMysticArray);
+    
 }
